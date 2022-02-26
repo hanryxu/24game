300a3c1757ed790c9cb500856281f13d59567d86
jtung069 <51225780+jtung069@users.noreply.github.com>
2022-02-25 16:53:59 -0800

    Add KYPD, ucf files
    KYPD = the number pad

---
+++ Num24.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:20:08 02/26/2022 
+// Design Name: 
+// Module Name:    Num24 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Num24(clk, JA, btnr, btnl, red, green, blue, hsync, vsync);
+	// User controls
+	input clk, JA, btnr, btrnl;
+	// VGA stuff
+	output wire [2:0] red;
+	output wire [2:0] green;
+	output wire [1:0] blue;
+	output wire hsync, vsync;
+endmodule
