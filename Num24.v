773a53e112952218cf8e181b4efffd832b7a1c42
jtung069 <51225780+jtung069@users.noreply.github.com>
2022-02-25 17:21:22 -0800

    Add clock_gen, Modify Num24

--- Num24.v
+++ Num24.v
@@ -18,12 +18,44 @@
 // Additional Comments: top module
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Num24(clk, JA, btnr, btnl, red, green, blue, hsync, vsync);
+module Num24(clk, JA, START, RESTART, red, green, blue, hsync, vsync);
 	// User controls
-	input clk, JA, btnr, btrnl;
+	input clk, JA, START, RESTART;
 	// VGA stuff
 	output wire [2:0] red;
 	output wire [2:0] green;
 	output wire [1:0] blue;
 	output wire hsync, vsync;
+	
+	// clock generation
+	// TODO: delete 1 hz, 2 hz clock, and clk_blink, add clk_pix
+	wire clk_1Hz;
+   wire clk_2Hz;
+   wire clk_blink;
+   wire clk_debounce;
+	
+   clock_gen clock_gen_INS(
+                  .clk_in(clk),
+                  .clk_1hz(clk_1Hz),
+                  .clk_2hz(clk_2Hz),
+                  .clk_blink(clk_blink),
+                  .clk_display(clk_debounce)
+              );
+	
+	// Number pad logic
+	wire [3:0] decode; // binary rep of hexadecimal digits --> F = 4'b1111
+	PmodKYPD num_pad(.clk(clk), .JA(JA), .Decode(decode));
+	
+	// Button debouncing
+	wire START_d;
+	debouncer debounce_START(.clk_in(clk_debounce), .but(START),
+											.but_out(START_d));
+	wire RESTART_d;
+	debouncer debounce_RESTART(.clk_in(clk_debounce), .but(RESTART),
+											.but_out(RESTART_d));
+											
+	// FSM
+	
+	// VGA Stuff
+	
 endmodule
